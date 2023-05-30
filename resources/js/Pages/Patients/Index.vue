584fe8656517bccfddae897b9f8e1997df44fe4c
Jinsik Kim <jkdev1@protonmail.com>
2023-05-30 13:16:22 -0400

    styling

--- resources/js/Pages/Patients/Index.vue
+++ resources/js/Pages/Patients/Index.vue
@@ -55,13 +55,13 @@ const deletePatient = () => {
                         </Link>
                     </div>
 
-                    <table class="table-auto mt-4 w-full">
+                    <table class="table-fixed mt-4 w-full">
                         <thead>
                         <tr>
                             <th class="text-left">ID</th>
                             <th class="text-left">Last Name</th>
                             <th class="text-left">First Name</th>
-                            <th class="text-center">Manage</th>
+                            <th class="text-left">Manage</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -70,7 +70,7 @@ const deletePatient = () => {
                             <td class="">{{ patient.last_name }}</td>
                             <td class="">{{ patient.first_name }}</td>
                             <td class="">
-                                <div class="flex justify-center items-center space-x-4">
+                                <div class="flex items-center space-x-4">
                                     <Link :href="route('patients.show', { patient: patient.id })">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
