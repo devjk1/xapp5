8ebf68f084d3c3630dd8bb2bf70c00426285c0d4
Jinsik Kim <jkdev1@protonmail.com>
2023-07-13 15:01:01 -0400

    remove uneeded

--- resources/js/Components/SearchInput.vue
+++ resources/js/Components/SearchInput.vue
@@ -12,7 +12,6 @@ const emits = defineEmits([
 
 <template>
     <input
-        :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         class="p-2 border-gray-800 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
     >
